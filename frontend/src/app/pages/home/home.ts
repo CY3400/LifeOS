3d3f50fd9f0af0e70941e173da5e45df5e1a5be7
Charbel Yammine <charbelymn@gmail.com>
2026-04-01 12:49:13 +0300

    Ajout de la partie dashboard

--- frontend/src/app/pages/home/home.ts
+++ frontend/src/app/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from "@angular/common";
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import { Api, DailyTask, Goal } from "../../services/api";
 
 @Component({
     selector: 'app-home',
@@ -9,6 +10,31 @@ import { FormsModule } from "@angular/forms";
     standalone: true,
     imports: [CommonModule, FormsModule]
 })
-export class Home {
+export class Home implements OnInit {
+    goals: Goal[] = [];
+    dailyTasks: DailyTask[] = [];
+    totalTasks: number = 0;
+    completedTasks: number = 0;
+    completionRate: number = 0;
 
+    constructor(private api: Api) {}
+
+    ngOnInit() {
+        this.loadDashboard();
+    }
+
+    loadDashboard(){
+        this.api.today().subscribe({
+            next: (dashboard) => {
+                this.goals = dashboard.goals;
+                this.dailyTasks = dashboard.dailyTasks;
+                this.totalTasks = dashboard.totalTasks;
+                this.completedTasks = dashboard.completedTasks;
+                this.completionRate = dashboard.completionRate;
+            },
+            error: () => {
+                console.error("Error lors du chargement du dashboard");
+            }
+        })
+    }
 }
