63d32995f0cb45c69d4b870fa354e3bb6212de78
Charbel Yammine <charbelymn@gmail.com>
2026-06-11 17:50:40 +0300

    Remove @Repository

--- backend/src/main/java/com/charbel/lifeos/repository/UserRepository.java
+++ backend/src/main/java/com/charbel/lifeos/repository/UserRepository.java
@@ -3,11 +3,8 @@ package com.charbel.lifeos.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import com.charbel.lifeos.entity.User;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmailIgnoreCase(String email);
 
